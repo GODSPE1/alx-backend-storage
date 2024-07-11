cf1fe18e4ec367ba62e95679b12d290f4ef7e612
GODSPE1 <godspy18@gmail.com>
2024-07-11 23:58:37 +0100

    creates a table called users without failing

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,8 +3,8 @@
 -- email: string
 -- name: string
 
-CREATE TABLE IF EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `users` (
     `id` INT NULL AUTO_INCREMENT PRIMARY KEY,
-    `email` VARCHAR(255) NOT NULL UNIQUE
+    `email` VARCHAR(255) NOT NULL UNIQUE,
     `name` VARCHAR(255)
 );
