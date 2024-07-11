2a3dbc44be663cb8575e9a78522e9a86736fd0d3
GODSPE1 <godspy18@gmail.com>
2024-07-12 00:50:50 +0100

    add not to script that creates a table users

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,7 +4,7 @@
 -- name: string
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     `name` VARCHAR(255),
     country ENUM('US', 'CO', 'TN') NOT NULL,
