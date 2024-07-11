1ff21bc492cb783b066888ecb560a0c40e701019
GODSPE1 <godspy18@gmail.com>
2024-07-11 23:23:48 +0100

    creates a table called users without failing

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+-- Script that creates a table called users without failing
+-- id INT
+-- email: string
+-- name: string
+
+CREATE TABLE IF EXISTS `users` (
+    `id` INT NULL AUTO_INCREMENT PRIMARY KEY,
+    `email` VARCHAR(255) NOT NULL UNIQUE
+    `name` VARCHAR(255)
+);
