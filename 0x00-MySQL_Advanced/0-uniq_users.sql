432885f76081509d3df7d857ed58d74a93b89193
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2024-07-12 00:17:59 +0100

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,8 +3,9 @@
 -- email: string
 -- name: string
 
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` INT NULL AUTO_INCREMENT PRIMARY KEY,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `name` VARCHAR(255)
-);
+CREATE TABLE IF NOT EXISTS users (
+    id INT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+    );
