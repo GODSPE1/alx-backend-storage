717713dc7483008585b2719fd94f4c5fcf3c22d5
GODSPE1 <godspy18@gmail.com>
2024-07-11 23:50:35 +0100

    SQL script that creates a table users following these requirements

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,11 @@
+-- Script that creates a table called users without failing
+-- id INT
+-- email: string
+-- name: string
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
+);
