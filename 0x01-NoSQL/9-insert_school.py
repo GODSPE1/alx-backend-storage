0085868695b68c650928a024befdd80a8cb8b4d6
GODSPE1 <godspy18@gmail.com>
2024-07-15 23:10:42 +0100

    function that inserts a new document in a

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python3
 """ This module define a function that inserts a new document in a
 collection based on kwargs
 """
 
 
-def list_all(mongo_collection):
-    """Python function that inserts a new document in a 
-    collection based on kwargs:
+def list_all(mongo_collection, **kwargs):
+    """
+    Parameters
+        mongo_collection: pymongo collectionobject
+        kwargs: dictionary with data to insert
+
+    Return
+        _id of the new document
     """
 
     return mongo_collection.insert_one(kwargs).insert_id
