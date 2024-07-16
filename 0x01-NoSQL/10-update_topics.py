c4ac8e84d79e1602cd6c4605cd3cffab7d4ceab8
GODSPE1 <godspy18@gmail.com>
2024-07-16 04:44:29 +0100

    efines a function that changes all topics of

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,9 +5,9 @@ a school document based on the name
 
 def update_topics(mongo_collection, name, topics):
     """
-    Parameter
+    Parameters
         mongo_collection: pymongo collection object
         name: school name to update
         topics: list of topics approached in the school
-        """
-    mongo_collection.update_many({"name": name}, {"$set": {topics: "topics"}});
+    """
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
