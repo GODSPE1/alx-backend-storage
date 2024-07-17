a494c5e0b7e2305efa03e6b3b658bfc27d8c4531
GODSPE1 <godspy18@gmail.com>
2024-07-18 00:21:49 +0100

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,10 +5,50 @@ Writing and Reading to/from Redis and Recovering original type
 
 
 import uuid
-from typing import Union
+from functools import wraps
+from typing import Callable, Union
 import redis
 
+def count_history(method: Callable) -> Callable:
+    """Function calls the history decorator"""
 
+      key = method.__qualname__ # type: ignore
+
+
+@wraps(method)
+def wrapper(self, *args, **kwargs):
+    """Wrapper function to"""
+
+    input_key = method.__qualname__ + ":inputs"
+    output_key = method.__qualname__ + ":outputs"
+
+    output = method(self, *args, **kwargs)
+
+    self._redis.rpush(input_key, str(args))
+    self._redis.rpush(output_key, str(output))
+    return output
+
+    return wrapper
+
+
+
+def count_calls(method: Callable) -> Callable:
+    """"""
+    key = method.__qualname__ # type: ignore
+
+@wraps(method)
+def wrapper(self, *args, **kwargs):
+    """Wrapper function to"""
+
+    self.redis.incr(key)
+    return method(self, *args, **kwargs)
+return wrapper
+
+
+def replay(fn: Callable):
+    """"""
+    with redis.Redis() as rds:
+        function_name = fn.__qualname__ 
 class Cache:
     """Writing strings to redis"""
 
@@ -23,3 +63,18 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """Reading from redis and recovering original type"""
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+    
+    def get_str(self, key: str) -> str:
+        """Gets string data type"""
+        return self.get(key: str)
+    
+    def get_int(self, key: int) -> int:
+        """Gets string data type"""
+        return self.get(key: int)
+    
